2416746e3764934e9b3437fd31212106aa61cf4f
referenc2 <denis-smirnov-2004@list.ru>
2025-10-05 16:30:20 +0000

    my_commit

---
+++ script_for_vpn.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+	echo "This script must be run as root"
+	exit 1
+fi
+
+dpkg --status openvpn &> /dev/null
+
+if [ $? -eq 0 ]; then
+	echo "openvpn: Already installed"
+else
+	sudo apt-get install -y openvpn
+fi
+
+cp /usr/share/doc/openvpn/examples/sample-config-files/server.conf /etc/openvpn/server/
+
+sed -ie '/tls-auth ta.key 0/a\tls-crypt ta.key 
+s/tls-auth ta.key 0/;tls-auth ta.key 0/
+/cipher AES-256-CBC/a\auth SHA256
+/cipher AES-256-CBC/a\cipher AES-256-GCM
+s/cipher AES-256-CBC/;cipher AES-256-CBC/
+/dh dh2048.pem/a\dh none
+s/dh dh2048.pem/;dh dh2048.pem/
+s/;user nobody/user nobody/
+s/;group nobody/group nobody/' /etc/openvpn/server/server.conf
+
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+
+sysctl -p
+
+systemctl -f enable openvpn-server@server.service
+systemctl start openvpn-server@server.service
+
+
